e9d8300c06013eeced738f78a7f1f5103ef0b888
Vesta080283 <43104215+Vesta080283@users.noreply.github.com>
2018-09-22 21:56:47 +0600

    Update myApp.js
    Update myApp

--- myApp.js
+++ myApp.js
@@ -1,27 +1,42 @@
-
 var express = require('express');
+//var bodyParser = require('body-parser');
+
 var app = express();
 
-// --> 7)  Mount the Logger middleware here
 
+// --> 7)  Mount the Logger middleware here
+/**app.use(function(req,res,next){
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+});*/
 
 // --> 11)  Mount the body-parser middleware  here
 
-
 /** 1) Meet the node console. */
+//console.log("Hello World");
 
 
 /** 2) A first working Express Server */
+/**app.get("/", (req, res) => {
+  res.send("Hello Express");
+});*/
 
 
 /** 3) Serve an HTML file */
-
+/**app.get("/", function(req, res) {
+        res.sendFile( __dirname + "/views/index.html");
+  });*/
 
 /** 4) Serve static assets  */
-
+app.use(express.static(process.cwd() + '/public'));
 
 /** 5) serve JSON on a specific route */
-
+/**app.use('/json',function(req,res){
+  if (process.env.MESSAGE_STYLE == "uppercase")
+    res.json({"message": "Hello json".toUpperCase()});
+  else
+    res.json({"message": "Hello json"});
+});*/
 
 /** 6) Use the .env file to configure the app */
  
@@ -32,13 +47,55 @@ var app = express();
 
 /** 8) Chaining middleware. A Time server */
 
+/**app.get('/now',function(req,res,next){
+  req.time = new Date().toString();
+  next();
+},
+  function(req,res){
+  res.json({time: req.time});
+});*/
 
 /** 9)  Get input from client - Route parameters */
 
+/**app.get('/:word/echo', function(req,res){
+  res.json({echo: req.params.word});
+});*/
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
+/**app.route('/name').get(function(req,res){
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  if (!firstName)
+    {
+    res.json({Error: "First name isn't specifyed"});
+      return;
+    }
+  if (!lastName)
+    {
+    res.json({Error: "Last name isn't specifyed"});
+      return;
+    }
+  
+  res.json({ name: firstName + ' ' + lastName});
+}).post(bodyParser.urlencoded({extended:false}), function(req,res){
+  var firstName = req.body.first;
+  var lastName = req.body.last;
+  
+  if (!firstName)
+    {
+    res.json({Error: "First name isn't specifyed"});
+      return;
+    }
+  if (!lastName)
+    {
+    res.json({Error: "Last name isn't specifyed"});
+      return;
+    }
+  
+  res.json({ name: firstName + ' ' + lastName});
+});*/
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
